b1c536add51f6f4aae7d7324f37f271c9746b3a4
Marek <marwis95@o2.pl>
2017-10-31 13:42:59 +0100

    Dziala

--- Restarter/Form1.cs
+++ Restarter/Form1.cs
@@ -6,7 +6,6 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
-using System;
 using System.IO;
 
 namespace Restarter
@@ -20,22 +19,28 @@ namespace Restarter
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            int i;
 
-            try
-            {   // Open the text file using a stream reader.
-                using (StreamReader sr = new StreamReader("TestFile.txt"))
+                using (StreamReader sr = new StreamReader("C:/counter.txt"))
                 {
                     // Read the stream to a string, and write the string to the console.
                     String line = sr.ReadToEnd();
+                    i = Int32.Parse(line);
                     Console.WriteLine(line);
                 }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("The file could not be read:");
-                Console.WriteLine(e.Message);
-            }
 
+            //MessageBox.Show(i.ToString());
+            i = i + 1;
+
+
+            string s = i.ToString();
+            byte[] data = Encoding.UTF8.GetBytes(s);
+            string path = @"C:/counter.txt";
+            FileStream fs = new System.IO.FileStream(path, FileMode.Create);
+            fs.Write(data, 0, data.Length);
+            fs.Close();
+            this.Close();
         }
     }
+
 }
